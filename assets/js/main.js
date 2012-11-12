8b31d3b7570ba781f19befb4f141032d4ad18235
interactivellama <llama@interactivellama.com>
2012-11-12 14:46:01 -0500

    Update styles, JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,22 @@ jQuery(function() {
 "use strict";
 // start here
 
+var oldie = false;
+
+if($.browser.msie && parseInt($.browser.version, 10) != 7) {
+	oldie = true;
+}
+
+if(!oldie) {
+
+}
 		
 });
 
+window.onload = function() {
+  $(window).smartresize()
+};
 
+$(document).ready(function() {
+
+});
